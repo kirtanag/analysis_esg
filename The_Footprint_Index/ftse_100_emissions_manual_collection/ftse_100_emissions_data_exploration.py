f3162df72ca1e4b4c1e476943bd9befcffa9f156
kirtanagopakumar <91904976+kirtanagopakumar@users.noreply.github.com>
2024-02-08 01:06:54 +0000

    Final manual emission captire

--- The_Footprint_Index/ftse_100_emissions_manual_collection/ftse_100_emissions_data_exploration.py
+++ The_Footprint_Index/ftse_100_emissions_manual_collection/ftse_100_emissions_data_exploration.py
@@ -1,6 +1,5 @@
 import sqlite3
-# import sqlite3
-#
+
 # # Connect to the SQLite database
 # conn = sqlite3.connect('../sql_databases/ghg_emissions.db')
 #
@@ -8,7 +7,7 @@ import sqlite3
 # cursor = conn.cursor()
 #
 # # Define the SQL query to update 'BP' to 'bp'
-# query = "UPDATE ghg_emissions SET unit = 'TeCO2e' WHERE unit = 'TeCO2c';"
+# query = "UPDATE ghg_emissions SET profit_before_tax = 337 WHERE ghg_id = 'SGE2022';"
 #
 # # Execute the SQL query
 # cursor.execute(query)
@@ -34,7 +33,7 @@ for row in rows:
 
 print(count)
 
-# # query2 = "DELETE FROM ghg_emissions WHERE ghg_id = 'VOD2023';"
+# query2 = "DELETE FROM ghg_emissions WHERE code = 'PSH';"
 # cursor.execute(query2)
 
 conn.close()
