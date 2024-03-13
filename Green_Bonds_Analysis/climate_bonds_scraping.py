0ac54a3dc7c7e4eafa1a0f1a363d2f82ff068a51
kirtanagopakumar <91904976+kirtanagopakumar@users.noreply.github.com>
2024-03-13 00:55:37 +0000

    Climate Bonds Scraping Script

---
+++ Green_Bonds_Analysis/climate_bonds_scraping.py
@@ -0,0 +1,100 @@
+from bs4 import BeautifulSoup as bs
+from requests import get
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, List
+
+
+# Defining the data class
+@dataclass
+class ClimateBondData:
+    bond_id: str
+    entity: str
+    amount_issued: int
+    currency: str
+    issue_date: datetime
+    maturity_date: Optional[datetime] = None
+    cbi_certified: Optional[str] = None
+    spo_provider: Optional[str] = None
+
+
+# Scraping the climate bonds website
+link = 'https://www.climatebonds.net/cbi/pub/data/bonds?items_per_page=All'
+# data = get_climate_data(link = link)
+
+
+# Defining function to scrape data
+def get_climate_data(link: str) -> List[ClimateBondData]:
+    page_data = get(link).text
+    soup = bs(page_data, 'html.parser')
+
+    html_data_list = soup.find_all('tr', {'class': ['odd', 'even', 'odd views-row-first',
+                                                    'even views-row-last']})
+    print(f'Items found- {len(html_data_list)}')
+    climate_data_list = []
+    for html_data in html_data_list:
+        climate_data = ClimateBondData(bond_id=get_bond_id(html=html_data),
+                                       entity=get_entity(html=html_data),
+                                       amount_issued=get_amount_issued(html=html_data),
+                                       currency=get_currency(html=html_data),
+                                       issue_date=get_issue_date(html=html_data),
+                                       maturity_date=get_maturity_date(html=html_data),
+                                       cbi_certified=get_cbi_certified(html=html_data),
+                                       spo_provider=get_spo_provider(html=html_data)
+                                       )
+        climate_data_list.append(climate_data)
+
+
+def get_bond_id(html: bs):
+    bond_id = html.find('td', class_ ='views-field views-field-title views-align-left').text.strip()
+    return bond_id
+
+def get_entity(html: bs):
+    entity = html.find('td', class_ ='views-field views-field-field-bond-entity').text.strip()
+    return entity
+
+def get_amount_issued(html: bs):
+     try:
+         amount_issued = int((html.find('td', class_ ='views-field views-field-field-bond-amt-issued views-align-right')
+                          .text.replace(',', '')).strip())
+     except:
+         amount_issued = None
+     return amount_issued
+
+def get_currency(html: bs):
+    currency = html.find('td', class_ ='views-field views-field-field-bond-currency views-align-left').text.strip()
+    return currency
+
+def get_issue_date(html: bs):
+    issue_date = html.find('td', class_ ='views-field views-field-field-bond-issue-date active views-align-right').text.strip()
+    issue_date_str = "01-" + issue_date
+    date_obj = datetime.strptime(issue_date_str, "%d-%b-%y")
+    return date_obj
+
+def get_maturity_date(html: bs):
+    try:
+        maturity_date = html.find('td', class_ ='views-field views-field-field-bond-maturity-date').text.strip()
+        maturity_date_str = "01-" + maturity_date
+        date_obj = datetime.strptime(maturity_date_str, "%d-%b-%y")
+        return date_obj
+    except:
+        return None
+
+def get_cbi_certified(html: bs):
+    try:
+        cbi_certified = html.find('td', class_ ='views-field views-field-field-bond-verifier').text.strip()
+        return cbi_certified
+    except:
+        return None
+
+def get_spo_provider(html: bs):
+    try:
+        spo_provider = html.find('td', class_ ='views-field views-field-field-bond-spo-provider').text.strip()
+        return spo_provider
+    except:
+        return None
+def main():
+    get_climate_data(link=link)
+
+if __name__ == "__main__":
+    main()
