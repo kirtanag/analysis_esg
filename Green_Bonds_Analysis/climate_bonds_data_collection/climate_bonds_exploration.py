d67baaa7109c8fa3c5e41744a5524ea2a54a66d6
kirtanagopakumar <91904976+kirtanagopakumar@users.noreply.github.com>
2024-03-23 21:55:47 +0000

    Edits

--- Green_Bonds_Analysis/climate_bonds_data_collection/climate_bonds_exploration.py
+++ Green_Bonds_Analysis/climate_bonds_data_collection/climate_bonds_exploration.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 # Connect to SQL
 conn = sqlite3.connect('../sql_databases/climate_bonds_data.db')
 cursor = conn.cursor()
@@ -13,5 +14,14 @@ for row in rows:
     print(row)
 
 print(count)
+
+
+# Export DataFrame to Excel file
+query = "SELECT * FROM climate_bonds_data;"
+df = pd.read_sql_query(query, conn)
+excel_file_path = '/Users/kirtanagopakumar/PycharmProjects/analysis_esg/Green_Bonds_Analysis/sql_databases/climate_bonds_data.xlsx'
+df.to_excel(excel_file_path, index=False)
+
+print("Data exported to Excel file:", excel_file_path)
 
 conn.close()
